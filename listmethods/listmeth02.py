73541ff6d3abc6d2d827dca5e2ba47f3aaf49efc
Greg Watt <wattssup@users.noreply.github.com>
2022-05-25 12:36:21 +0000

    manipulating lists

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+protoa = ["ssh", "http", "https"]
+print(proto)
+proto.append("dns") # this line will add "dns" to the end of our list
+protoa.append("dns") # this line will add "dns" to the end of our list
+print(proto)
+proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+proto.extend(proto2) # pass proto2 as an argument to the extend method
+print(proto)
+protoa.append(proto2) # pass proto2 as an argument to the append method
+print(protoa)
+
+# Counts the number of http's  in proto and displays that number
+print("the number of http's in proto is " + str(proto.count("http")))
+
+
