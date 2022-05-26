72ce21c9e320fa7e6c4d3d4950eb2a53bb9bab6d
Greg Watt <wattssup@users.noreply.github.com>
2022-05-26 15:59:14 +0000

    Outputting files

---
+++ credmaker/makecred.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os.system
+
+choice = int(input("Enter 1 if you want to be prompted for input, or 2 to run from the CSV File: "))
+
+if choice == 1:
+    system(./openstackrc_maker.py)
+else:
+    isystem(./rclooper.py)
+
+
